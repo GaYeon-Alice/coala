df1c37d2013e1137894e1df869fa99a9bcdb441f
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2024-08-28 21:15:33 +0900

    Update path.py

--- chatbot/path.py
+++ chatbot/path.py
@@ -10,5 +10,5 @@ answer_path = "/home/user/coala/data/answer.txt"
 # CodeLlama 모델 경로
 base_model_path = "TinyPixel/CodeLlama-7B-Python-bf16-sharded"
 
-#PEFT 모델 경로
+# PEFT 모델 경로
 peft_model_path = "kgyalice/llama2_peft"
